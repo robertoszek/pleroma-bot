28c41e263147758c87445b62955383d24ab30200
robertoszek <tabulacion@gmail.com>
2021-01-16 22:24:59 +0100

    Separate stdout and stderr logging

--- pleroma_bot/__init__.py
+++ pleroma_bot/__init__.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import logging
 
 __version__ = "0.6.8"
@@ -7,17 +8,22 @@ log_path = os.path.join(os.getcwd(), "error.log")
 logging.root.setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
 
-c_handler = logging.StreamHandler()
+c_handler = logging.StreamHandler(sys.stdout)
+e_handler = logging.StreamHandler(sys.stderr)
 f_handler = logging.FileHandler(log_path)
 c_handler.setLevel(logging.INFO)
+e_handler.setLevel(logging.ERROR)
 f_handler.setLevel(logging.ERROR)
 
 c_format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+e_format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
 f_format = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
 c_handler.setFormatter(c_format)
+e_handler.setFormatter(c_format)
 f_handler.setFormatter(f_format)
 
 logger.addHandler(c_handler)
+logger.addHandler(e_handler)
 logger.addHandler(f_handler)
 
 from .cli import *
