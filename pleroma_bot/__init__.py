3142bae4b86e6a3a4bc70b7b6aedf2fb40536941
robertoszek <robertoszek@robertoszek.xyz>
2021-01-25 02:18:10 +0100

    Added ANSI colors to logger formatter

--- pleroma_bot/__init__.py
+++ pleroma_bot/__init__.py
@@ -10,6 +10,32 @@ class StdOutFilter(logging.Filter):
         return rec.levelno in (logging.WARNING, logging.DEBUG, logging.INFO)
 
 
+class CustomFormatter(logging.Formatter):
+    grey = "\x1b[38;21m"
+    yellow = "\x1b[33;21m"
+    red = "\x1b[31;21m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format_r = "%(asctime)s - %(name)s - %(levelname)s - %(message)s "
+    format_l = (
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%("
+        "filename)s:%(lineno)d) "
+    )
+
+    FORMATS = {
+        logging.DEBUG: grey + format_r + reset,
+        logging.INFO: grey + format_r + reset,
+        logging.WARNING: yellow + format_l + reset,
+        logging.ERROR: red + format_l + reset,
+        logging.CRITICAL: bold_red + format_l + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
 log_path = os.path.join(os.getcwd(), "error.log")
 logging.root.setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
@@ -21,7 +47,7 @@ c_handler.setLevel(logging.INFO)
 e_handler.setLevel(logging.ERROR)
 f_handler.setLevel(logging.ERROR)
 
-c_format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+c_format = CustomFormatter()
 e_format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
 f_format = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
 c_handler.setFormatter(c_format)
