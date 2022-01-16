b55309e72e5ddcc2c8e77c6e2d5865cedccce4b5
robertoszek <robertoszek@robertoszek.xyz>
2022-01-16 23:03:04 +0100

    order archive tweets retrieved by created_time

--- pleroma_bot/_utils.py
+++ pleroma_bot/_utils.py
@@ -529,17 +529,23 @@ def process_archive(archive_zip_path, start_time=None):
         "meta": {}
     }
     for tweet in tweets_archive:
+        created_at = tweet["tweet"]["created_at"]
+        created_at_f = datetime.strftime(
+            datetime.strptime(created_at, "%a %b %d %H:%M:%S +0000 %Y"),
+            "%Y-%m-%dT%H:%M:%SZ",
+        )
         if start_time:
-            created_at = tweet["tweet"]["created_at"]
-            created_at_f = datetime.strftime(
-                datetime.strptime(created_at, "%a %b %d %H:%M:%S +0000 %Y"),
-                "%Y-%m-%dT%H:%M:%SZ",
-            )
             if created_at_f < start_time:
                 continue
         tweet["tweet"]["text"] = tweet["tweet"]["full_text"]
-        tweet["tweet"]["possibly_sensitive"] = False
+        tweet["tweet"]["created_at"] = created_at_f
+        if "possibly_sensitive" not in tweet["tweet"].keys():
+            tweet["tweet"]["possibly_sensitive"] = False
         tweets["data"].append(tweet["tweet"])
+    # Order it just in case
+    tweets["data"] = sorted(
+        tweets["data"], key=lambda i: i["created_at"], reverse=True
+    )
     return tweets
 
 
