54979027dd0d5f023a07193de85e62838cdb3b54
robertoszek <robertoszek@robertoszek.xyz>
2022-11-17 00:26:31 +0100

    rollback req for py3.6

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setuptools.setup(
     },
     python_requires='>=3.6',
     install_requires=[
-        'requests>=2.28.1',
+        'requests>=2.24.0',
         'PyYAML>=5.3.1',
         'requests-oauthlib>=1.3.0',
         'feedparser>=6.0.10',
